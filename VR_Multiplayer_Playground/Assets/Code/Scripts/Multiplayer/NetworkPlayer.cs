3e22575fff628cb79f8c22b9f7f28afbc9e01e3f
Maxwell Lovell <maxwellloganlovell@gmail.com>
2022-03-08 17:01:42 -0500

    [Create] NetworkPlayer visuals and spawner

---
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayer.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+
+public class NetworkPlayer : MonoBehaviour
+{
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    private PhotonView photonView;
+
+    private void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+
+        if (!photonView.IsMine)
+            return;
+
+        head.gameObject.SetActive(false);
+        rightHand.gameObject.SetActive(false);
+        leftHand.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!photonView.IsMine)
+            return;
+
+        MapPosition(head, XRNode.Head);
+        MapPosition(leftHand, XRNode.LeftHand);
+        MapPosition(rightHand, XRNode.RightHand);
+    }
+
+    void MapPosition(Transform target, XRNode node)
+    {
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+        
+        target.SetPositionAndRotation(position, rotation);
+    }
+}
---
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayerSpawner.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnedPlayerPrefab;
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}
