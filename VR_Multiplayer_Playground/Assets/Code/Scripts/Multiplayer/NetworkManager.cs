ce8e7de2def346f558bfd3aa9a1d8cc13e14feca
Maxwell Lovell <maxwellloganlovell@gmail.com>
2022-03-08 15:09:27 -0500

    [Create] Added photon assets and created sample scene

---
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkManager.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        ConnectToServer();
+    }
+
+    void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect to server");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server.");
+        base.OnConnected();
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 3;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Room 1", roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a Room");
+        base.OnJoinedRoom();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
