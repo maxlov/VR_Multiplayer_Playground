85739bd83ac16e1f6d4b6a322c423d9d927a76fa
Maxwell Lovell <maxwellloganlovell@gmail.com>
2022-03-09 19:43:13 -0500

    [Update] changed networkplayer to track transform positions instead of nodes

--- VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayer.cs
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayer.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Unity.XR.CoreUtils;
 using Photon.Pun;
 
 public class NetworkPlayer : MonoBehaviour
@@ -7,14 +8,24 @@ public class NetworkPlayer : MonoBehaviour
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
+
     private PhotonView photonView;
+
+    private Transform headOrigin;
+    private Transform leftHandOrigin;
+    private Transform rightHandOrigin;
 
     private void Start()
     {
         photonView = GetComponent<PhotonView>();
+        XROrigin origin = FindObjectOfType<XROrigin>();
 
         if (!photonView.IsMine)
             return;
+
+        headOrigin = origin.transform.Find("Camera Offset/Main Camera");
+        leftHandOrigin = origin.transform.Find("Camera Offset/LeftHand Controller");
+        rightHandOrigin = origin.transform.Find("Camera Offset/RightHand Controller");
 
         head.GetChild(0).gameObject.SetActive(false);
         rightHand.GetChild(0).gameObject.SetActive(false);
@@ -26,16 +37,13 @@ public class NetworkPlayer : MonoBehaviour
         if (!photonView.IsMine)
             return;
 
-        MapPosition(head, XRNode.Head);
-        MapPosition(leftHand, XRNode.LeftHand);
-        MapPosition(rightHand, XRNode.RightHand);
+        MapPosition(head, headOrigin);
+        MapPosition(leftHand, leftHandOrigin);
+        MapPosition(rightHand, rightHandOrigin);
     }
 
-    void MapPosition(Transform target, XRNode node)
-    {
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
-        InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
-        
-        target.SetPositionAndRotation(position, rotation);
+    void MapPosition(Transform target, Transform originTransform)
+    {        
+        target.SetPositionAndRotation(originTransform.position, originTransform.rotation);
     }
 }
