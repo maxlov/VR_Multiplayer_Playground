f8432213e2ab1a58f7dff95ecee827fe1ae47376
epaschke22 <ericksonpaschke@gmail.com>
2022-03-21 01:30:06 -0400

    [Update] Created Physics Button
    Also moved around locations of scripts for organization

---
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Input/PhysicsButton.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PhysicsButton : MonoBehaviour
+{
+    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    private bool _isPressed;
+    private Vector3 _startPos;
+    private ConfigurableJoint _joint;
+
+    public UnityEvent onPressed, onReleased;
+
+    void Start()
+    {
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
+    }
+
+    void Update()
+    {
+        if (!_isPressed && GetValue() + threshold >= 1)
+        {
+            Pressed();
+        }
+        if (_isPressed && GetValue() - threshold <= 0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        float value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+        if (Mathf.Abs(value) < deadZone)
+            value = 0;
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        _isPressed = true;
+        onPressed.Invoke();
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+        onReleased.Invoke();
+    }
+}
