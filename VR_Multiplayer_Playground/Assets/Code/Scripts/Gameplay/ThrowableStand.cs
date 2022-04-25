ce9abf3e81529fa11870d750f429e45d28190485
epaschke22 <ericksonpaschke@gmail.com>
2022-04-25 11:30:16 -0400

    [Update] Added Hats and made another fix for throwables

--- VR_Multiplayer_Playground/Assets/Code/Scripts/Gameplay/ThrowableStand.cs
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Gameplay/ThrowableStand.cs
@@ -7,8 +7,8 @@ using Photon.Pun;
 
 public class ThrowableStand : MonoBehaviour
 {
-    public Transform spawnPoint;
     [SerializeField] private string throwableName;
+    [SerializeField] private Transform spawnPoint;
 
     [SerializeField] private GameObject loadingUI;
     [SerializeField] private Slider loadingBar;
@@ -16,7 +16,6 @@ public class ThrowableStand : MonoBehaviour
 
     private bool startLoading = false;
 
-    private Throwable _throwableScript;
     private PhotonView photonView;
 
 
@@ -35,6 +34,7 @@ public class ThrowableStand : MonoBehaviour
     public void NetworkLoadBar()
     {
         photonView.RPC("LoadBar", RpcTarget.All);
+        photonView.RPC("ToggleSocket", RpcTarget.All, false);
     }
 
     [PunRPC]
@@ -54,26 +54,24 @@ public class ThrowableStand : MonoBehaviour
 			{
                 startLoading = false;
                 loadingUI.SetActive(false);
+                photonView.RPC("ToggleSocket", RpcTarget.All, true);
                 SpawnObject();
             }
         }
     }
+
+    [PunRPC]
+    void ToggleSocket(bool toggle)
+	{
+        spawnPoint.GetComponent<Collider>().enabled = toggle;
+	}
 
     void SpawnObject()
 	{
         if (!PhotonNetwork.IsMasterClient)
             return;
         var spawned = PhotonNetwork.Instantiate(throwableName, spawnPoint.position, spawnPoint.rotation);
-        photonView.RPC("SendRefs", RpcTarget.All, spawned.GetComponent<PhotonView>().ViewID);
-        Debug.Log(spawned.GetComponent<PhotonView>().ViewID + " clientside");
-    }
-
-    [PunRPC]
-    public void SendRefs(int spawnedID)
-	{
-        Debug.Log(spawnedID + " RPC method called");
-        GameObject spawned = PhotonView.Find(spawnedID).gameObject;
-        if (spawned.TryGetComponent<Throwable>(out _throwableScript))
-            _throwableScript.throwableStand = this;
+        //if (spawned.TryGetComponent<Throwable>(out _throwableScript))
+        //    _throwableScript.throwableStand = this;
     }
 }
