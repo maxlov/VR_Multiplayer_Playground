7eedcf3b89badbaa7d3ff139abe4acdf44c93ce6
Maxwell Lovell <maxwellloganlovell@gmail.com>
2022-04-26 13:05:34 -0400

    [Update] Made hats persist for network player through level change

--- VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayer.cs
+++ VR_Multiplayer_Playground/Assets/Code/Scripts/Multiplayer/NetworkPlayer.cs
@@ -115,6 +115,7 @@ public class NetworkPlayer : MonoBehaviour
     [PunRPC]
      void RPC_ChooseHat(int index)
 	{
+        Debug.Log("Choosing hat " + index);
 		for (int i = 0; i < hats.childCount; i++)
 		{
             if (i == index && !photonView.IsMine)
